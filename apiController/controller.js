893c5787701f7e2193cb42f246e6010a4b87500e
je-lopez <je.lopez@me.com>
2017-04-24 18:19:14 -0700

    update controller for posting

--- apiController/controller.js
+++ apiController/controller.js
@@ -31,64 +31,67 @@ let questions =  {
     'Content-type': 'application/json',
     'Authorization': 'Bearer ya29.Gls2BOhmJGsI9W_wY3lhaaSXkKvUHKN7fCNKiqJUjHPdVtBzEkGndl1ifkpbd0YYrUTYwK9ueeypgAHcxJcw_BEskYY8wn5ocUSfUXjtiTHQGjTmj406D7CZbwmB'
   },
-  'owners': ['jelopez0005@gmail.com'],
-  'wantedResponseCount': 100,
-  'audience': {
-    'country': 'US',
-    'languages': ['en-US']
+  body: {
+    'owners': ['jelopez0005@gmail.com'],
+    'wantedResponseCount': 100,
+    'audience': {
+      'country': 'US',
+      'languages': ['en-US']
+    },
+    'questions': [
+      {
+        'question': 'What is your favorite programming language?',
+        'type': 'singleAnswer',
+        'answers': [
+          'Python',
+          'Ruby',
+          'JavaScript',
+          'Swift'
+        ]
+      },
+      {
+        'question': 'How many hours do you code each day?',
+        'type': 'singleAnswer',
+        'answers': [
+          '4',
+          '6',
+          '8',
+          'More than 8'
+        ]
+      },
+      {
+        'question': 'What is your favorite API?',
+        'type': 'singleAnswer',
+        'answers': [
+          'Google Surveys API',
+          'Facebook Live API',
+          'Amazon Web Services API',
+          'Twitter API'
+        ]
+      },
+      {
+        'question': 'What is your favorite taco spot?',
+        'type': 'singleAnswer',
+        'answers': [
+          '29th St and Fruitvale Ave',
+          'Tacos Mi Rancho',
+          'Cholita Linda',
+          'Taco Bell'
+        ]
+      },
+      {
+        'question': 'What is your favorite type of coffee?',
+        'type': 'singleAnswer',
+        'answers': [
+          'Drip',
+          'French Press',
+          'Espresso',
+          'Iced'
+        ]
+      }
+    ]
   },
-  'questions': [
-    {
-      'question': 'What is your favorite programming language?',
-      'type': 'singleAnswer',
-      'answers': [
-        'Python',
-        'Ruby',
-        'JavaScript',
-        'Swift'
-      ]
-    },
-    {
-      'question': 'How many hours do you code each day?',
-      'type': 'singleAnswer',
-      'answers': [
-        '4',
-        '6',
-        '8',
-        'More than 8'
-      ]
-    },
-    {
-      'question': 'What is your favorite API?',
-      'type': 'singleAnswer',
-      'answers': [
-        'Google Surveys API',
-        'Facebook Live API',
-        'Amazon Web Services API',
-        'Twitter API'
-      ]
-    },
-    {
-      'question': 'What is your favorite taco spot?',
-      'type': 'singleAnswer',
-      'answers': [
-        '29th St and Fruitvale Ave',
-        'Tacos Mi Rancho',
-        'Cholita Linda',
-        'Taco Bell'
-      ]
-    },
-    {
-      'question': 'What is your favorite type of coffee?',
-      'type': 'singleAnswer',
-      'answers': [
-        'Drip',
-        'French Press',
-        'Espresso',
-        'Iced'
-      ]
-    }
-  ]
+  json: true
 }
 
 
@@ -107,9 +110,14 @@ const apiController = {
     .then(resp => {
       const token = JSON.parse(resp).access_token
       questions.headers.Authorization = 'Bearer ' + token
+      console.log('before we send')
       return rp(questions)
     })
-    .then(console.log)
+    .then((data) => {
+      console.log('smaller parts after!')
+      // JSON.stringify(data)
+      console.log(JSON.stringify(data))
+    })
   }
 }
 
